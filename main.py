a74f56781e81034c54d17d3a10e96f258d31ccd4
novaticker <lds811311@gmail.com>
2025-07-29 11:38:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,3 @@ def delete_news():
         return jsonify({'status': 'success'})
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)})
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
